89d14da8ee6e415cc8dadfccc70a0122afdb9998
zicoprmd <zicoprmd@gmail.com>
2022-08-18 20:13:59 +0700

    added background every pages and sidebar

--- pages/2_♋_ Februari.py
+++ pages/2_♋_ Februari.py
@@ -1,6 +1,39 @@
 import pandas as pd
 import streamlit as st
 import plotly.express as px
+import base64
+
+@st.experimental_memo
+def get_img_as_base64(file):
+    with open(file, 'rb') as f:
+        data = f.read()
+    return base64.b64encode(data).decode()
+
+img = get_img_as_base64('./img/image2.jpg')
+
+page_bg_img = f'''
+<style>
+[data-testid="stAppViewContainer"] {{
+background-image: url('https://images.unsplash.com/photo-1660756010411-be7aed1b88cb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2050&q=80');
+background-size: cover;
+}}
+
+[data-testid='stHeader'] {{
+    background-color: rgba(0, 0, 0, 0);
+}}
+
+[data-testid='stToolbar'] {{
+right: 2rem;
+}}
+
+[data-testid='stSidebar'] > div:first-child {{
+    background-image: url('data:image/png;base64,{img}');
+    background-size: cover;
+}}
+</style>
+'''
+
+st.markdown(page_bg_img, unsafe_allow_html=True)
 
 st.title('Laporan IGD Februari')
 
@@ -31,7 +64,7 @@ st.plotly_chart(bar_chart)
 
 ### --- PIE CHARTS
 pie_chart = px.pie(df_februari,
-                    title='Presentasi Penyakit iGD',
+                    title='Presentasi Penyakit IGD',
                     names='Diagnosa 1')
 
 st.plotly_chart(pie_chart)
--- pages/🌈_Portofolio.py
+++ pages/🌈_Portofolio.py
@@ -1,10 +1,45 @@
 import streamlit as st # pip install streamlit
 from streamlit_option_menu import option_menu
+import base64
+
+st.set_page_config(page_title='Portofolio', page_icon='🌈', layout='centered', initial_sidebar_state='collapsed')
+
+@st.experimental_memo
+def get_img_as_base64(file):
+    with open(file, 'rb') as f:
+        data = f.read()
+    return base64.b64encode(data).decode()
+
+img = get_img_as_base64('./img/image2.jpg')
+
+page_bg_img = f'''
+<style>
+[data-testid="stAppViewContainer"] {{
+background-image: url('https://images.unsplash.com/photo-1660756010411-be7aed1b88cb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2050&q=80');
+background-size: cover;
+}}
+
+[data-testid='stHeader'] {{
+    background-color: rgba(0, 0, 0, 0);
+}}
+
+[data-testid='stToolbar'] {{
+right: 2rem;
+}}
+
+[data-testid='stSidebar'] > div:first-child {{
+    background-image: url('data:image/png;base64,{img}');
+    background-size: cover;
+}}
+</style>
+'''
+
+st.markdown(page_bg_img, unsafe_allow_html=True)
+
 
 # 1. as a sidebar menu
 
 # 2. horizontal bar
-
 selected = option_menu(
         menu_title=None, #required
         options=['Home', 'Projects', 'Contact', 'Gallery'], #required
@@ -12,6 +47,7 @@ selected = option_menu(
         menu_icon='cast', #optional
         default_index=0, #optional
         orientation='horizontal',
+        
     )
 
 if selected == "Home":
@@ -19,7 +55,7 @@ if selected == "Home":
     st.subheader('Hi, I am Zico Permadi, MD :wave:')
     st.title('A Data Analyst From Tangerang')
     st.write('I am passionate about finding ways to use Python to be more efficient')
-    st.write('[Learn More >](https://zicoprmd.github.io/)')
+    st.write('[Learn More >>](https://zicoprmd.github.io/)')
 if selected == "Projects":
     st.title(f"{selected}")
 if selected == "Contact":
