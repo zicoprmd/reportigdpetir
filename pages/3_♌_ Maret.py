f12fc35d7676310bef3c7aaa1f8af1f9b9ff752d
zicoprmd <zicoprmd@gmail.com>
2022-08-18 20:56:27 +0700

    change

--- pages/3_♌_ Maret.py
+++ pages/3_♌_ Maret.py
@@ -1,32 +1,6 @@
 import pandas as pd
 import streamlit as st
 import plotly.express as px
-import base64
-
-@st.experimental_memo
-def get_img_as_base64(file):
-    with open(file, 'rb') as f:
-        data = f.read()
-    return base64.b64encode(data).decode()
-
-img = get_img_as_base64('./img/image2.jpg')
-
-page_bg_img = f'''
-<style>
-
-
-[data-testid='stToolbar'] {{
-right: 2rem;
-}}
-
-[data-testid='stSidebar'] > div:first-child {{
-    background-image: url('data:image/png;base64,{img}');
-    background-size: cover;
-}}
-</style>
-'''
-
-st.markdown(page_bg_img, unsafe_allow_html=True)
 
 st.title('Laporan IGD Februari')
 
