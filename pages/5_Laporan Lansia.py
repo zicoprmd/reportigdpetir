600d7f2daf8c99fefae697b8a584f8f2b39b8c43
zicoprmd <zicoprmd@gmail.com>
2023-01-28 10:23:00 +0700

    added grafik lansia chart target 2023

--- pages/5_Laporan Lansia.py
+++ pages/5_Laporan Lansia.py
@@ -9,6 +9,7 @@ page_bg_img = f'''
 #MainMenu {{visibility: hidden;}}
 footer {{visibility: hidden;}}
 [data-testid="stAppViewContainer"] {{
+backgroudn-image: url('')
 background-size: cover;
 }}
 
@@ -28,15 +29,29 @@ st.markdown(page_bg_img, unsafe_allow_html=True)
 ### --- LOAD DATAFRAME JIWA
 excel_file = 'excel/LANSIA2022.xlsx'
 sheet_name = 'LANSIA2022'
+sheet_target = 'TARGET2023'
+
 
 df = pd.read_excel(excel_file, sheet_name=sheet_name,
                     usecols='A:D', nrows=15)
 
 ### BAR CHART
-
-bar_chart = px.bar(df, title='Lansia 2022',
+bar_chart = px.bar(df, title='laporan lansia 2022',
                     x='Bulan',
                     y=['Pra Lansia', 'Lansia', 'Lansia Risti'],
-                    template='seaborn')
+                    barmode='group',
+                    template='seaborn',
+                    text_auto=True)
 
 st.plotly_chart(bar_chart)
+
+st.subheader('Target sasaran Lansia tahun 2023')
+
+df_target = pd.read_excel(excel_file, sheet_name=sheet_target,
+                            usecols='A:B', nrows=15)
+
+### bar chart
+
+bar_chart_target = px.bar(df_target, x='umur', y='target', text_auto=True)
+
+st.plotly_chart(bar_chart_target)
